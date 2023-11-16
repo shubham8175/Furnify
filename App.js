5a0f74464f23a4743479944a523abb85703076a9
shubham8175 <shubham7868175@gmail.com>
2023-11-17 00:26:50 +0530

    fonts installed

--- App.js
+++ App.js
@@ -4,11 +4,17 @@ import React from 'react'
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text style={styles.text}>Hiii</Text>
+      <Text style={styles.text}>Hiii my name is Shubham</Text>
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  text:{
+    fontSize:30,
+    fontFamily:'Poppins-ExtraBold'
+  }
+})
